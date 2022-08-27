e03bface3da63812ea60dcbdd03ebac102d25da4
EdisonAltamirano <A00825234@itesm.mx>
2022-08-26 20:39:12 -0500

    FIx

--- demo_interface.py
+++ demo_interface.py
@@ -40,12 +40,13 @@ class Missions:
 class Button:
     def __init__(self, text,  pos, font):
         self.x, self.y = pos
-        self.size=(100,50)
+        self.size=(120,50)
         self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
         self.title = text
         self.font = pygame.font.SysFont("Arial", font)
         self.color = 'black'
         self.oldcolor='black'
+        self.mymission=Missions()
     def show(self):
         self.surface = pygame.Surface(self.size)
         self.text = self.font.render(self.title, 1, pygame.Color("White"))
@@ -62,13 +63,39 @@ class Button:
             if pygame.mouse.get_pressed()[0]:
                 if self.rect.collidepoint(x, y):
                     self.show()
- 
-SERVER_HOST = "10.22.240.51"
+                    self.mymission.change_mission()
+                    self.send_info()
+    def send_info(self):
+        if not c.is_open():
+            if not c.open():
+                    print("unable to connect to "+SERVER_HOST+":"+str(SERVER_PORT))
+        if c.is_open():
+            #Read Battery / RobotStatus /Distance (Modbus)
+            bits = c.read_holding_registers(0, 12)
+            self.mymission.inforobot[3]=bits[3]
+            self.mymission.inforobot[4] = bits[4]
+            self.mymission.inforobot[5] = bits[5] 
+            print(bits)
+            #Map coordinate location to pi
+            theta = np.radians(13)
+            offsetx=7.9115512
+            offsety=2.50132282
+            rheight=11.29203556
+            rwidth=17.69995916
+            c1, s = np.cos(theta), np.sin(theta)
+            R = np.array(((c1, -s), (s, c1)))
+            #Convert bit +-
+            self.mymission.inforobot[10]=int(mapping(bits[6]+offsetx,screen_x,rwidth))
+            self.mymission.inforobot[11]=int(mapping(bits[7]+offsety,screen_y,rheight))
+            #Update registers self.mymissionStatus,Goalx,Goaly
+            c.write_multiple_registers(0,[self.mymission.inforobot[0],self.mymission.inforobot[1],self.mymission.inforobot[2]])
+            print("Si")
+            time.sleep(1)
+SERVER_HOST = "10.22.216.59"
 SERVER_PORT = 12345
 c = ModbusClient()
 c.host(SERVER_HOST)
 c.port(SERVER_PORT)
-mission=Missions()
 def mapping(x,game,ros):
     return (x*game)/ros
 def mainloop():
@@ -83,36 +110,14 @@ def mainloop():
     button2.show()
     button3.show()
     while True:
-        if not c.is_open():
-            if not c.open():
-                    print("unable to connect to "+SERVER_HOST+":"+str(SERVER_PORT))
-        if c.is_open():
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                button1.click(event)
-                button2.click(event)
-                button3.click(event)
-            #Read Battery / RobotStatus /Distance (Modbus)
-            bits = c.read_holding_registers(0, 12)
-            mission.inforobot[3]=bits[3]
-            mission.inforobot[4] = bits[4]
-            mission.inforobot[5] = bits[5] 
-            #Map coordinate location to pi
-            theta = np.radians(13)
-            offsetx=7.9115512
-            offsety=2.50132282
-            rheight=11.29203556
-            rwidth=17.69995916
-            c1, s = np.cos(theta), np.sin(theta)
-            R = np.array(((c1, -s), (s, c1)))
-            #Convert bit +-
-            mission.inforobot[10]=int(mapping(bits[6]+offsetx,screen_x,rwidth))
-            mission.inforobot[11]=int(mapping(bits[7]+offsety,screen_y,rheight))
-            #Update registers MissionStatus,Goalx,Goaly
-            c.write_multiple_registers(0,[mission.inforobot[0],mission.inforobot[1],mission.inforobot[2]])
-            c.write_multiple_registers(8,[mission.inforobot[8],mission.inforobot[9],mission.inforobot[10],mission.inforobot[11]])
-            time.sleep(1)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            button1.click(event)
+            button2.click(event)
+            button3.click(event)
+
+            #c.write_multiple_registers(8,[mission.inforobot[8],mission.inforobot[9],mission.inforobot[10],mission.inforobot[11]])
         clock.tick(30)
         pygame.display.update()
  
